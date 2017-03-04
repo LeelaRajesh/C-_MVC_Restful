0f8f43ec383308049dde6cfddd94d75e9171d29b
LeelaRajesh <leelarajeshsayana@gmail.com>
2017-03-04 14:20:50 -0500

    Adding new rental DTO

---
+++ Application/Dtos/NewRentalDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVCappli_rest.Models;
+
+namespace MVCappli_rest.Dtos
+{
+    public class NewRentalDto
+    {
+        public List<int> MoiveId { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
