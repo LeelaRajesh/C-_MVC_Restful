a9ae887f86c3ad283af8fc4a255bb2b4c2e7ccf8
LeelaRajesh <leelarajeshsayana@gmail.com>
2017-03-04 14:23:15 -0500

    Adding new Api for new movie rentals

---
+++ Application/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MVCappli_rest.Models;
+using MVCappli_rest.Dtos;
+
+namespace MVCappli_rest.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //post api/newrentals
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            return Ok();
+        }
+    }
+}
